248e6c728166558f472481375a1e6d358073b6cf
Basit Obaid <basit_obaid@global.komatsu>
2022-12-30 14:16:16 +0500

    added parameter

--- lib/services/auth/auth_user.dart
+++ lib/services/auth/auth_user.dart
@@ -4,8 +4,12 @@ import 'package:flutter/foundation.dart';
 @immutable
 class AuthUser {
   final bool isEmailVerified;
+  final String? email;
 
-  const AuthUser(this.isEmailVerified);
+  const AuthUser({required this.email, required this.isEmailVerified});
 
-  factory AuthUser.fromFirebase(User user) => AuthUser(user.emailVerified);
+  factory AuthUser.fromFirebase(User user) => AuthUser(
+        email: user.email,
+        isEmailVerified: user.emailVerified,
+      );
 }
